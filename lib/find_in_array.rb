37d710711fd6a285fc89c2a4e3499d317dd69e92
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-22 17:42:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -7,9 +7,10 @@ def find_element_index(array, value_to_find)
     #binding.pry 
     if array[i] == value_to_find
       return i
+    else
+      return nil 
     end 
   end
-  return nil
 end
 
 puts find_element_index([1, 2, 3, 4], 5)
