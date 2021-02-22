80a702f4db6bd51f01876cad7ccb123c8e3fb0fa
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-22 17:34:50 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   #binding.pry 
   length = array.length 
   #binding.pry 
-  length.times do |i|
+  length.times-1 do |i|
     #binding.pry 
     if array[i] == value_to_find
       return i
